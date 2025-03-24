cbcdc85f9bc4643660a4b00353df38624d1c397b
unknown <boss.redgar@mail.ru>
2025-03-24 22:09:10 +0300

    feat: add decision 5 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const a = new Date(year, month, 0, 0, 0, 0);
+  return a.getDate();
 }
 
 /**
