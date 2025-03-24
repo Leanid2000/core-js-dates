8f19751928df1914edc35b7507f45bc398fe1ae0
unknown <boss.redgar@mail.ru>
2025-03-24 22:08:17 +0300

    feat: add decision 3 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const a = new Date(date);
+  const mass = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    0: 'Sunday',
+  };
+  const a2 = a.getDay();
+  return mass[a2];
 }
 
 /**
